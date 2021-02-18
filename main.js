0ef948f5345f67139608e6b9f1de5df414a49321
ClemBesacier <clementinebesacier@yahoo.fr>
2021-02-18 16:38:02 +0100

    result page + recette page

--- main.js
+++ main.js
@@ -16,3 +16,35 @@ $('.owl-home').owlCarousel({
         }
     }
 });
+
+// Ingredients 
+$('.product').click(function () {
+    $('.popup').addClass('active');
+});
+
+$('.closeCross').click(function () {
+    $('.popup').removeClass('active');
+});
+
+$('.each').click(function () {
+    $(this).toggleClass('validate');
+});
+
+// Slider recettes
+$('.owl-ing').owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: false,
+    dots: true,
+    responsive: {
+        0: {
+            items: 5
+        },
+        600: {
+            items: 7
+        },
+        1000: {
+            items: 9
+        }
+    }
+});
