24b74fe4fdcd11a2e721cd6057deec40bf05a46a
ClemBesacier <clementinebesacier@yahoo.fr>
2021-02-18 10:20:48 +0100

    homepage

---
+++ main.js
@@ -0,0 +1,18 @@
+// Slider homepage
+$('.owl-home').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    responsive: {
+        0: {
+            items: 2
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 4
+        }
+    }
+});
